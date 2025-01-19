80404dc2e548794f52d688a84cb1e1d57cf55e93
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2025-01-19 09:21:49 +0000

    feat: add solution for baekjoon 15688

---
+++ baekjoon/15688/15688.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+void solution(std::vector<int> &);
+
+int main(int argc, const char **argv) {
+  std::cin.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+
+  int n = 0;
+  std::cin >> n;
+
+  std::vector<int> numbers(n);
+  for (int &number : numbers) {
+    std::cin >> number;
+  }
+
+  solution(numbers);
+  for (const int &number : numbers) {
+    std::cout << number << '\n';
+  }
+
+  return 0;
+}
+
+void solution(std::vector<int> &numbers) {
+  std::sort(numbers.begin(), numbers.end());
+}
